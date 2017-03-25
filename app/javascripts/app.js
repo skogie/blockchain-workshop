63d6b8c6b19e051fa506f980c23750bf09b8c4da
simsko <simen.skoglund@knowit.no>
2017-03-25 21:09:53 +0100

    frontend workshop almost complete

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -17,6 +17,15 @@ var MetaCoin = contract(metacoin_artifacts);
 var accounts;
 var account;
 
+var populateDropdown = function(select, accounts) {
+    for(var i = 0; i < accounts.length; i ++) {
+      var option = document.createElement("option");
+      option.textContent = accounts[i];
+      option.value = accounts[i];
+      select.appendChild(option);
+    }
+}
+
 window.App = {
   start: function() {
     var self = this;
@@ -39,16 +48,28 @@ window.App = {
       accounts = accs;
       account = accounts[0];
 
-      self.refreshBalance();
+      //self.refreshBalance();
 
-      var select = document.getElementById("drop");
+      var select =  document.getElementById("drop");
+      var select2 = document.getElementById("sendDropdown");
+      var select3 = document.getElementById("createdropdown");
+      var select4 = document.getElementById("admindropdown");
+      var select5 = document.getElementById("fromdropdown");
+      var select6 = document.getElementById("todropdown");
+      var select7 = document.getElementById("employee");
+      var select8 = document.getElementById("payoutonnext");
 
-      for(var i = 0; i < accounts.length; i ++) {
-        var option = document.createElement("option");
-        option.textContent = accounts[i];
-        option.value = accounts[i];
-        select.appendChild(option);
-      }
+      populateDropdown(select, accounts); 
+      populateDropdown(select2, accounts);
+      populateDropdown(select3, accounts); 
+      populateDropdown(select4, accounts);
+      populateDropdown(select5, accounts); 
+      populateDropdown(select6, accounts); 
+      populateDropdown(select7, accounts); 
+      populateDropdown(select8, accounts); 
+
+ 
+
 
     });
   },
@@ -66,7 +87,7 @@ window.App = {
       meta = instance;
       return meta.getBalance.call(account, {from: account});
     }).then(function(value) {
-      var balance_element = document.getElementById("balance");
+      var balance_element = document.getElementById("balance2");
       balance_element.innerHTML = value.valueOf();
     }).catch(function(e) {
       console.log(e);
@@ -74,18 +95,149 @@ window.App = {
     });
   },
 
+  //    function createAccount(string _name, string _mail) {
+
+
+  createAccount: function() {
+    // var name = document.getElementById("name").value;
+    // var email = document.getElementById("email").value;
+    var self = this;
+
+    var createAddress = document.getElementById("createdropdown");
+    var address = createAddress.options[createAddress.selectedIndex].value;
+
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.createAccount.sendTransaction(/*name, email,*/ {from: address});
+    }).then(function() {
+      console.log(address);
+      self.setStatus("Account created!");
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error sending coin; see log.");
+    });
+
+  },
+
   sendCoin: function() {
     var self = this;
 
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
+    var amount = document.getElementById("amount2").value;
 
+    var fromA = document.getElementById("fromdropdown");
+    var fromAddress = fromA.options[fromA.selectedIndex].value;
+
+    var toA = document.getElementById("todropdown");
+    var toAddress = toA.options[toA.selectedIndex].value;
+
+    //createAndGiveMoneyToAllEmployees(int _amount)
     this.setStatus("Initiating transaction... (please wait)");
 
     var meta;
     MetaCoin.deployed().then(function(instance) {
       meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
+      return meta.sendCoin.sendTransaction(toAddress, amount, {from: fromAddress});
+    }).then(function() {
+      self.setStatus("Transaction complete!");
+      self.refreshBalance();
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error sending coin; see log.");
+    });
+  },
+
+  validateEmployee: function() {
+    var self = this;
+
+    var createAddress = document.getElementById("employee");
+    var address = createAddress.options[createAddress.selectedIndex].value;
+
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.validateEmployee.sendTransaction(address,{from: account});
+    }).then(function() {
+      self.setStatus("Transaction complete!");
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error getting balance; see log.");
+    });
+  },
+
+  isAccountAdmin: function() {
+    var self = this;
+    var createAddress = document.getElementById("admindropdown");
+    var address = createAddress.options[createAddress.selectedIndex].value;
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.isAccountAdmin(address);
+    }).then(function(admin) {
+      console.log(admin)
+      document.getElementById("isadmin").innerHTML = admin.valueOf();
+      self.setStatus("Transaction complete!");
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error sending coin; see log.");
+    });
+  },
+
+  createAndGiveMoneyToAllEmployees: function() {
+    var self = this;
+
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.createAndGiveMoneyToAllEmployees.sendTransaction(100, {from: account});
+    }).then(function() {
+      console.log(account)
+      self.setStatus("Transaction complete!");
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error sending coin; see log.");
+    });
+  },
+
+  createAndSendCoin: function() {
+    var self = this;
+
+    var amount = document.getElementById("amount").value;
+
+    var dropAddress = document.getElementById("sendDropdown");
+    var address = dropAddress.options[dropAddress.selectedIndex].value;
+
+    //createAndGiveMoneyToAllEmployees(int _amount)
+    this.setStatus("Initiating transaction... (please wait)");
+
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.createAndSendCoin.sendTransaction(address, amount, {from: account});
+    }).then(function() {
+      self.setStatus("Transaction complete!");
+      self.refreshBalance();
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error sending coin; see log.");
+    });
+  },
+
+  payOutOnNextSalary: function() {
+    var self = this;
+
+    var amount = document.getElementById("amount3").value;
+
+    var dropAddress = document.getElementById("payoutonnext");
+    var address = dropAddress.options[dropAddress.selectedIndex].value;
+
+    //createAndGiveMoneyToAllEmployees(int _amount)
+    this.setStatus("Initiating transaction... (please wait)");
+
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.payOutOnNextSalary.sendTransaction(amount, {from: address});
     }).then(function() {
       self.setStatus("Transaction complete!");
       self.refreshBalance();
@@ -97,19 +249,29 @@ window.App = {
 
   getCoin: function() {
     var self = this;
-    var amount = parseInt(document.getElementById("balance").value);
+    var amount = parseInt(document.getElementById("balance2").value);
     var meta;
     var dropAddress = document.getElementById("drop");
     var address = dropAddress.options[dropAddress.selectedIndex].value;
 
     MetaCoin.deployed().then(function(instance) {
       meta = instance;
-      return meta.getBalance(address);
+      return meta.getBalance.call(address, {from: address});
     }).then(function(balance) {
-      self.setStatus("Transaction complete!");
       document.getElementById("balance2").innerHTML = balance.valueOf();
-      console.log(balance.valueOf());
-      console.log(address);
+      return meta.accountExists.call({from: address});
+      //    function accountExists() constant returns(bool) {
+   }).then(function(name) {
+     self.setStatus("Transaction complete!");
+     document.getElementById("name1").innerHTML = name.valueOf();
+     console.log(name.valueOf());
+     console.log(address);
+     // return meta.getMail(address);
+//    }).then(function(email) {
+//      self.setStatus("Transaction complete!");
+//      document.getElementById("email1").innerHTML = email.valueOf();
+//      console.log(email.valueOf());
+//      console.log(address);
     }).catch(function(e) {
       console.log(e);
       self.setStatus("Error sending coin; see log.");
