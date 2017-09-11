0a129c19d413c07077ffd6113c3691d820ba6779
simsko <simen.skoglund@knowit.no>
2017-09-11 23:22:50 +0200

    Fixed validate employee test and renamed html etc

--- test/testcoinit.js
+++ test/testcoinit.js
@@ -9,48 +9,28 @@ contract('Coinit', function(accounts) {
     });
   });
 
-
+  
   it("should validate account correctly", function() {
 
     // Get initial balances of first and second account.
     var account_one = accounts[0];
     var account_two = accounts[1];
-    var account_three = accounts[2];
 
-    var account_one_starting_balance;
-    var account_two_starting_balance;
-    var account_one_ending_balance;
-    var account_two_ending_balance;
-    var account_three_ending_balance;
-
-    var amount = 10;
-
-    console.log("hei");
-
-    var meta;
+    var coinit;
     return Coinit.deployed().then(function(instance) {
-      meta = instance;
-      console.log("hei2");
-      return meta.isOwner.call();
+      coinit = instance;
+      return coinit.isOwner.call();
     }).then(function(isAdmin) {
-      console.log("is admin: " + isAdmin);
-      return meta.createAccount.sendTransaction("name", "mail", {from: account_two});
+        //Lager account-two
+        return coinit.createAccount.sendTransaction("name", "mail", {from: account_two});
       }).then(function() {
-        console.log("hei3");
-        return meta.validateEmployee.sendTransaction(account_three, {from: account_one});
-      }).then(function(error, success) {
-        return meta.getValidated.call(account_two, {from: account_two});
+        //validerer account-two fra admin
+        return coinit.validateAccount.sendTransaction(account_two, {from: account_one});
+      }).then(function() {
+        //validate account-two fra account-two
+        return coinit.getValidated.call(account_two, {from: account_two});
       }).then(function(validated) {
-        console.log(validated);
-        assert.equal(validated, false, "The account should not be validated since it has to be done by admin.");
-        return meta.validateEmployee.sendTransaction(account_two, {from: account_one});
-      }).then(function(success) {
-        console.log("success2");
-        assert.equal(success, true, "validateEmployee should return true");
-        return meta.getValidated.call(account_two, {from: account_two});
-      }).then(function(validated) {
-        console.log("validated2");
-        assert.equal(validated, true, "The account should be validated");       
+        assert.equal(validated, true, "The account should now be validated");          
       });
     });
 
@@ -69,22 +49,22 @@ contract('Coinit', function(accounts) {
 
     var amount = 10;
 
-    var meta;
+    var coinit;
     return Coinit.deployed().then(function(instance) {
-      meta = instance;
-      return meta.createAndSendCoin.sendTransaction(account_two, 10, {from: account_one});
+      coinit = instance;
+      return coinit.createAndSendCoin.sendTransaction(account_two, 10, {from: account_one});
       }).then(function() {
-        return meta.getBalance.call(account_two, {from: account_two});
+        return coinit.getBalance.call(account_two, {from: account_two});
       }).then(function(balance) {
           account_two_ending_balance = balance.valueOf();
           assert.equal(account_two_ending_balance, 10, "Amount wasn't correctly created and sent to the receiver");
       }).then(function() {
-        return meta.sendCoin.sendTransaction(account_three, 5, {from: account_two});
+        return coinit.sendCoin.sendTransaction(account_three, 5, {from: account_two});
       }).then(function() {
-        return meta.getBalance.call(account_two, {from: account_two});
+        return coinit.getBalance.call(account_two, {from: account_two});
       }).then(function(acc2balance) {
         account_two_ending_balance = acc2balance.valueOf();
-        return meta.getBalance.call(account_three, {from: account_three});
+        return coinit.getBalance.call(account_three, {from: account_three});
       }).then(function(acc3balance) {
         account_three_ending_balance = acc3balance.valueOf();                       
         assert.equal(account_two_ending_balance, 5, "Amount on account two wasnt 5");
